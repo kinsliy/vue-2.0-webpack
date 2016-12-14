30d0df528b62e1a5e4df3c9213bf005bedbcd0ae
kinsliy <2606938846@qq.com>
2016-12-14 18:44:22 +0800

    2

---
+++ hello.vue
@@ -0,0 +1,25 @@
+<template>
+	<p v-text='msg'></p>
+
+</template>
+
+<script >
+	module.exports={
+
+		  data:function(){
+		  	  return {
+		  	  	  msg:'你好',
+		  	  }
+		  },
+
+
+	}
+
+</script>
+<style scoped>
+	p{
+		font-size: 18px;
+		color: red;
+	}
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// var webpack =require('webapck');
+
+module.exports = {
+  // devtool: 'eval-source-map',
+  entry: {
+     
+     index:__dirname + "/main.js",
+  },
+  output: {
+    path: __dirname + "/public",//打包后的文件存放的地方
+    filename: "[name].js"//打包后输出文件的文件名
+  },
+  module: {//在配置文件里添加JSON loader
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      // { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ]
+  },
+  resolve: {
+
+  alias: {
+    'vue$': 'vue/dist/vue.js'
+  }
+}
+  
+   
+
+
+
+}
